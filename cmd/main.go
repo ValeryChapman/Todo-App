899a385091aa79d4d3a270b019210fe31c24e3b4
TheProgersTeam <68988841+ValeryChapman@users.noreply.github.com>
2021-08-15 01:02:35 +0300

    back #1

---
+++ cmd/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/ValeryChapman/todo-app"
+	"github.com/ValeryChapman/todo-app/pkg/handler"
+	"github.com/ValeryChapman/todo-app/pkg/repository"
+	"github.com/ValeryChapman/todo-app/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("Error initializing configs: %s", err.Error())
+	}
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	srv := new(todo.Server)
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+		log.Fatalf("Error occured while renning http server: %s", err.Error)
+	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}
